c27642a618143a2fe2df2ffac7964014af0022cb
Chunlei Wu <anewgene@yahoo.com>
2022-01-24 14:48:50 -0800

    build: :construction_worker: rename bin/nde-hub.py to bin/hub.py
    keep consistency with other hubs

--- biothings-hub/files/docker-entrypoint.sh
+++ biothings-hub/files/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 cd /home/biothings/nde-hub
 # replace PID 1 process with Python so it will receive the SIGINT when
 # people run `docker stop nde-hub`
-PYTHONPATH=/home/biothings/nde-hub exec /home/biothings/venv/bin/python /home/biothings/nde-hub/bin/nde-hub.py
+PYTHONPATH=/home/biothings/nde-hub exec /home/biothings/venv/bin/python /home/biothings/nde-hub/bin/hub.py
