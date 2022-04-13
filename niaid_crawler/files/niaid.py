c61e0cedc0e3aa55e47fe534b9b3a3d37f8105d3
Dylan Welzel <dylanwelzel@Dylans-MacBook-Pro.local>
2022-04-13 10:16:02 -0700

    converted to dates to iso, removed None values

--- niaid_crawler/files/niaid.py
+++ niaid_crawler/files/niaid.py
@@ -2,8 +2,7 @@ import json
 import requests
 import logging
 import validators
-
-# from pprint import pprint
+from datetime import datetime
 
 
 logging.basicConfig(level=logging.INFO)
@@ -32,68 +31,78 @@ def parse():
     count = 0
 
     for trial in trials:
-        try:
-            trial['name'] = trial.pop('title')
-            trial['identifier'] = trial.pop('cmc_unique_id')
-            trial['description'] = trial.pop('brief_summary')
-            trial['datePublished'] = trial.pop('data_availability_date')
-            trial['dateModified'] = trial.pop('most_recent_update')
-            trial['additionalType'] = trial.pop('data_available')
-            trial['funding'] = [{'funder': {'name': trial.pop('creator')}}]
-            trial['nctid'] = trial.pop('nct_number')
-            trial['infectiousDisease'] = trial.pop('condition')
-            trial['mainEntityOfPage'] = trial.pop('clinical_trial_website')
+        trial['name'] = trial.pop('title')
+        trial['identifier'] = trial.pop('cmc_unique_id')
+        trial['description'] = trial.pop('brief_summary')
 
-            # check if url is valid then take pubmed id
-            citation_URL = trial.pop('publications')
-            if citation_URL is not None and validators.url(citation_URL):
-                if 'pubmed' in citation_URL:
-                    trial['pmids'] = citation_URL.split('/')[-2]
-                # To convert doi id to pubmed id, use requests library to search doi id on pubmed search engine, catch redirect and take the pubmed id from url
-                elif 'doi' in citation_URL:
-                    doi_id = citation_URL.split('/')[-1]
-                    r = requests.get("https://pubmed.ncbi.nlm.nih.gov/?term=" + doi_id)
-                    if 'pubmed' in r.url:
-                        trial['pmids'] = r.url.split('/')[-2]
-                    else:
-                        trial['citation'] = None
+        trial['datePublished'] = trial.pop('data_availability_date')
+        if trial['datePublished'] == "Coming Soon":
+            trial['datePublished'] = None
+        if trial['datePublished'] is not None:
+            iso_date = datetime.strptime(trial['datePublished'], '%B %Y')
+            trial['datePublished'] = iso_date.strftime('%Y-%m')
+
+        trial['dateModified'] = trial.pop('most_recent_update')
+        if trial['dateModified'] is not None:
+           iso_date = datetime.strptime(trial['dateModified'], '%B %Y')
+           trial['dateModified'] = iso_date.strftime('%Y-%m')
+
+        trial['additionalType'] = trial.pop('data_available')
+        trial['funding'] = [{'funder': {'name': trial.pop('creator')}}]
+        trial['nctid'] = trial.pop('nct_number')
+        trial['infectiousDisease'] = trial.pop('condition')
+        trial['mainEntityOfPage'] = trial.pop('clinical_trial_website')
+
+        # check if url is valid then take pubmed id
+        citation_URL = trial.pop('publications')
+        if citation_URL is not None and validators.url(citation_URL):
+            if 'pubmed' in citation_URL:
+                trial['pmid'] = citation_URL.split('/')[-2]
+            # To convert doi id to pubmed id, use requests library to search doi id on pubmed search engine, catch redirect and take the pubmed id from url
+            elif 'doi' in citation_URL:
+                doi_id = citation_URL.split('/')[-1]
+                r = requests.get("https://pubmed.ncbi.nlm.nih.gov/?term=" + doi_id)
+                if 'pubmed' in r.url:
+                    trial['pmid'] = r.url.split('/')[-2]
                 else:
-                    trial['citation'] = [{'url':citation_URL}]
+                    trial['citation'] = None
             else:
-                trial['citation'] = None
+                trial['citation'] = [{'url':citation_URL}]
+        else:
+            trial['citation'] = None
 
-            if trial['data_available_for_request'] == 'true':
-                trial['data_available_for_request'] = "Restricted"
-            if trial['data_available_for_request'] == 'false':
-                trial['data_available_for_request'] = "Closed"
+        if trial['data_available_for_request'] == 'true':
+            trial['data_available_for_request'] = "Restricted"
+        if trial['data_available_for_request'] == 'false':
+            trial['data_available_for_request'] = "Closed"
 
-            trial['conditionsOfAccess'] = trial.pop(
-                'data_available_for_request')
+        trial['conditionsOfAccess'] = trial.pop(
+            'data_available_for_request')
 
-            # de-duplication of identifier
-            if trial['identifier'] != trial['nctid']:
-                trial['identifier'] = [trial['identifier'], trial['nctid']]
-            else:
-                trial['identifier'] = [trial['nctid']]
+        # de-duplication of identifier
+        if trial['identifier'] != trial['nctid']:
+            trial['identifier'] = [trial['identifier'], trial['nctid']]
+        else:
+            trial['identifier'] = [trial['nctid']]
 
-            # unique _id appending identifier
-            trial['_id'] = "accessclinicaldata_" + trial['identifier'][0]
-            trial['includedInDataCatalog'] = {
-                "name": "AccessClinicalData@NIAID"
-            }
-            trial['@type'] = "Dataset"
-            trial['url'] = "https://accessclinicaldata.niaid.nih.gov/study-viewer/clinical_trials/" + \
-                trial['identifier'][0]
+        # unique _id appending identifier
+        trial['_id'] = "accessclinicaldata_" + trial['identifier'][0]
+        trial['includedInDataCatalog'] = {
+            "name": "AccessClinicalData@NIAID"
+        }
+        trial['@type'] = "Dataset"
+        trial['url'] = "https://accessclinicaldata.niaid.nih.gov/study-viewer/clinical_trials/" + \
+            trial['identifier'][0]
 
-            yield trial
+        result = {k: v for k, v in trial.items() if v is not None}
+        missing_properties = {k: v for k, v in trial.items() if v == None}
 
-            count += 1
-            logger.info("Parsed %s records", count)
-        except StopIteration:
-            logger.info("Finished Parsing. Total Records: %s", count)
-            # if StopIteration is raised, break from loop
-            break
+        yield result
 
-# parse()
-# for i in parse():
-#     pprint(i)
+        count += 1
+
+        if len(missing_properties.keys()) > 0:
+            logger.warning("Missing type transformation: {}".format(str(missing_properties.keys())))
+        logger.info("Parsed %s records", count)
+
+    logger.info("Finished Parsing. Total Records: %s", count)
