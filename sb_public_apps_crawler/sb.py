03668ef33f6cfe4f2ee7bdda53014847b6a37f22
Dylan Welzel <dylanwelzel@gmail.com>
2022-04-15 14:15:47 -0700

    first pass at transformations

--- sb_public_apps_crawler/sb.py
+++ sb_public_apps_crawler/sb.py
@@ -31,9 +31,58 @@ def parse():
             "@type": "Dataset",
             "url": "https://igor.sbgenomics.com/public/apps/" + identifier
         }
+        if app_class := data.get('class'):
+            output['applicationCategory'] = app_class
+        if cwl_version := data.get('cwlVersion'):
+            output['version'] = cwl_version
+        if label := data.get('label'):
+            output['name'] = label
+        if description := data.get('description'):
+            output['description'] = description
+        if doc := data.get('doc'):
+            output['description'] = doc
+        if inputs := data.get('inputs'):
+            output['bioschemas:input'] = inputs
+        if outputs := data.get('outputs'):
+            output['bioschemas:input'] = outputs
+        if steps := data.get('steps'):
+            output['bioschemas:hasPart'] = steps
+        if requirements := data.get('requirements'):
+            output['schema:softwareRequirements'] = requirements
+        if image_url := data.get('sbg:image_url'):
+            output['thumbnailUrl'] = image_url
+        if toolkit := data.get('sbg:toolkit'):
+            output['applicationSuite'] = toolkit
+        if license := data.get('license'):
+            output['license'] = license
+        if links := data.get('sbg:links'):
+            output['codeRepository'] = links
+        if categories := data.get('sbg:categories'):
+            output['applicationSubCategory'] = categories
+        if revisions_info := data.get('sbg:revisionsInfo'):
+            output['softwareVersion'] = revisions_info
+        # if project_name := data.get('sbg:projectName'):
+        if tool_author := data.get('sbg:toolAuthor'):
+            output['creator'] = tool_author
+        if app_version := data.get('appVersion'):
+            output['version'] = app_version
+        if modified_on := data.get('sbg:modifiedOn'):
+            output['dateModified'] = modified_on
+        if created_on := data.get('sbg:createdOn'):
+            output['dateCreated'] = created_on
+        if contributors := data.get('sbg:contributors'):
+            output['contributor'] = contributors
+        if publisher := data.get('sbg:publisher'):
+            output['sdPublisher'] = publisher
+        if workflow_language := data.get('sbg:workflowLanguage'):
+            output['programmingLanguage'] = workflow_language
+            
         yield output
 
 
+json_list = []
 for i in parse():
-    with open('json_data.json', 'a') as outfile:
-        json.dump(i, outfile)
+    json_list.append(i)
+
+with open('json_data.json', 'w') as outfile:
+    json.dump(json_list, outfile)
