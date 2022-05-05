71fc8f0687ed18152328909cd03f47e075f3536c
jal347 <linjason.03@gmail.com>
2022-05-05 13:04:07 -0700

    implemented caching ncbi_geo

--- ncbi_geo_crawler/files/delete-cache.sh
+++ ncbi_geo_crawler/files/delete-cache.sh
@@ -2,11 +2,16 @@
 
 # Deletes the scrapy cache of the spider
 # https://github.com/scrapy/scrapy/issues/3204
-# I have no clue if there is a better way of getting the spider name other than this way
-NAME=$(grep 'name' spider.py | cut -d "'" -f 2)
-DIR=/home/jalin/cache/$NAME/*
+(
+    cd /home/biothings || exit
+    # I have no clue if there is a better way of getting the spider name other than this way
+    # we can manually write in the spider name if no other solution is found
+    # NAME=$(grep 'name' /home/biothings/spider.py | cut -d "'" -f 2)
+    # temp solution to pass as a parameter
+    DIR=/cache/$1
 
-if [ -d $DIR ]; then
-  echo "Removing cache"
-  rm -rf $DIR
-fi
+    if [ -d $DIR ]; then
+      flock --verbose --nonblock /crawler.lock \
+      -c "echo 'Removing /cache/$1' ; rm -rf $DIR && echo '/cache/$1 removed'"
+    fi
+) > /proc/1/fd/1 2> /proc/1/fd/2
