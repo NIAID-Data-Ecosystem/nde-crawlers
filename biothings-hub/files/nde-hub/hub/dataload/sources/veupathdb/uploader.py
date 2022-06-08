b1c8e675bf0b9a9f6118f4a492bd1e11d63d7b6d
Nichollette <nichollette.aco@gmail.com>
2022-06-08 11:33:34 -0400

    mod for pmid helper integrated, datasource ready

--- biothings-hub/files/nde-hub/hub/dataload/sources/veupathdb/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/veupathdb/uploader.py
@@ -1,4 +1,10 @@
 from hub.dataload.nde import NDESourceUploader
+from utils.pmid_helper import load_pmid_ctfd
 
 class VEuPathDB_Uploader(NDESourceUploader):
     name = "veupathdb"
+
+    def load_data(self, data_folder):
+        docs = load_pmid_ctfd(data_folder)
+        for doc in docs:
+            yield doc
