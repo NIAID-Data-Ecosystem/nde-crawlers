1cc8e51257642861667dc943e5bbe7bb818ff7a9
Nichollette <nichollette.aco@gmail.com>
2022-06-22 10:57:36 -0400

    update to date mod

--- clinepidb_crawler/files/spider.py
+++ clinepidb_crawler/files/spider.py
@@ -2,7 +2,10 @@ import scrapy
 import requests 
 import xmltodict 
 import logging
-import json
+from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
 logger = logging.getLogger('nde-logger')
 
 class ClinEpiDBSpider(scrapy.Spider):
@@ -12,22 +15,26 @@ class ClinEpiDBSpider(scrapy.Spider):
     sitemap_urls = ['https://clinepidb.org/ce/sitemap.xml']
     #start_urls = ['https://clinepidb.org/ce/app/record/dataset/DS_010e5612b8']
 
-    # get the dataset ids
-    def get_ids(self):
+    def start_requests(self):
         xml_url = "https://clinepidb.org/ce/sitemap-SitemapDatasets.xml"
-        id_response = requests.get(xml_url)
-        dict_data = xmltodict.parse(id_response.content)
-        url_list = [hit['loc'] for hit in dict_data["urlset"]["url"]]
+        sitemap_request = requests.get(xml_url)
+        dict_data = xmltodict.parse(sitemap_request.content)
+        urls = [hit['loc'] for hit in dict_data["urlset"]["url"]]
+        test_url = urls[0]
         #id_dict = [hit['loc'].split("/")[-1] for hit in dict_data['urlset']['url']]
         logger.info("[INFO] Completed clinepidb dataset id list")
-        return url_list
-    
-    def start_requests(self):
-        urls = self.get_ids()
-        test_url = urls[0]
-        self.test_id = test_url.split("/")[-1]
+        test_id = test_url.split("/")[-1]
 
-        #for url in urls[:3]:
+        for _url in urls[:3]:
+            yield SeleniumRequest(
+                url=_url
+                wait_time=10,
+                callback=self.parse,
+                wait_until=presence_of_element_located(
+                    (By.XPATH,
+                    '')
+                )
+            )
         yield scrapy.Request(test_url, callback=self.parse)
 
     def parse(self, response): 
@@ -35,8 +42,6 @@ class ClinEpiDBSpider(scrapy.Spider):
         #data = json.loads(response.body)
         #body = response.xpath('//li[contains(@class,"main-stack") and contains(a/text(), "Interior")]/div//text()').extract()
         #body = response.xpath('//div[@class="wdk-DataTableCellContent"]').extract()
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
-    
+        #with open(filename, 'wb') as f:
+            #f.write(response.body)
 
