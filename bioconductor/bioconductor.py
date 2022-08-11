d6e68252416c1cb3b9b8fda8f835e1185b7434dd
Dylan Welzel <dylanwelzel@gmail.com>
2022-08-11 13:41:39 -0700

    r script

--- bioconductor/bioconductor.py
+++ bioconductor/bioconductor.py
@@ -1,16 +1,9 @@
-import scrapy
+import pandas as pd
+from rpy2.robjects.packages import importr
 
-
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    def start_requests(self):
-        url = 'https://bioconductor.org/packages/3.15/bioc/html/a4.html'
-        yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+biocPkgTools = importr('BiocPkgTools')
+biocPkgList = biocPkgTools.biocPkgList()
+biocPkgList = pd.DataFrame(biocPkgList)
+# save each row as a separate dictionary with column names as keys
+biocPkgList = biocPkgList.to_dict('records')
+print(biocPkgList)
