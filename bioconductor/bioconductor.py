636f47b1fd07f1caf54331c16c5fa5f3b63ceecc
Dylan Welzel <dylanwelzel@gmail.com>
2022-08-11 15:00:53 -0700

    getting columns

--- bioconductor/bioconductor.py
+++ bioconductor/bioconductor.py
@@ -1,9 +1,9 @@
+import json
 import pandas as pd
 from rpy2.robjects.packages import importr
 
 biocPkgTools = importr('BiocPkgTools')
 biocPkgList = biocPkgTools.biocPkgList()
-biocPkgList = pd.DataFrame(biocPkgList)
-# save each row as a separate dictionary with column names as keys
-biocPkgList = biocPkgList.to_dict('records')
-print(biocPkgList)
+df = pd.DataFrame(biocPkgList)
+
+print(df.get_lo)
