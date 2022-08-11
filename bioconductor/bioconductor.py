7fec77301877ed27f7b3162ce44d8c1f35bc4eed
Dylan Welzel <dylanwelzel@gmail.com>
2022-08-11 16:05:40 -0700

    using importr

--- bioconductor/bioconductor.py
+++ bioconductor/bioconductor.py
@@ -1,9 +1,12 @@
 import json
 import pandas as pd
 from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+
 
 biocPkgTools = importr('BiocPkgTools')
 biocPkgList = biocPkgTools.biocPkgList()
-df = pd.DataFrame(biocPkgList)
-
-print(df.get_lo)
+# save bioctools package list to a csv file
+# convert to pandas dataframe
+biocPkgList = pd.DataFrame(biocPkgList)
+biocPkgList.to_csv('biocPkgList.csv')
