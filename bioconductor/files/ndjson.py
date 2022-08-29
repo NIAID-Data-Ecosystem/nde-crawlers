6d9247077031e4d6a4723ecac647022c945fd72e
Dylan Welzel <dylanwelzel@gmail.com>
2022-08-29 10:07:54 -0700

    revert ndjson

--- bioconductor/files/ndjson.py
+++ bioconductor/files/ndjson.py
@@ -14,7 +14,7 @@ release_string = datetime.datetime.now(
     datetime.timezone.utc
 ).strftime('%Y-%m-%dT%H:%M:%SZ')
 dirname = os.path.join(
-    'd/data', 'bioconductor_crawled'
+    '/data', 'bioconductor_crawled'
 )
 os.makedirs(dirname, exist_ok=True)
 release_filename = os.path.join(
