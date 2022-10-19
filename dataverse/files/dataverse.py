bfa884e5ba4e253c1820e4959c9698fb66eff1a6
Nichollette <nichollette.aco@gmail.com>
2022-10-19 12:59:43 -0400

    adding dataverse working file

---
+++ dataverse/files/dataverse.py
@@ -0,0 +1,251 @@
+import datetime
+import time
+import json
+import logging
+import requests
+from html.parser import HTMLParser
+from sql_database import NDEDatabase
+
+#import sys
+#sys.path.append("/Users/nacosta/Documents/nde-crawlers/dataverse")
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(name)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger('nde-logger')
+
+class Dataverse(NDEDatabase):
+    SQL_DB = "dataverse.db"
+    DATAVERSE_SERVER = "https://dataverse.harvard.edu/api"
+    DATA_URL = f"{DATAVERSE_SERVER}/search?q=data"
+    EXPORT_URL = f"{DATAVERSE_SERVER}/datasets/export?exporter=schema.org"
+
+    def get_all_datasets_from_dataverses(self):
+        logger.info("finding all dataverses data available")
+        #dataverses = self.find_relevant_dataverses()
+        dataverse_endpoint = "https://dataverse.harvard.edu/api/search?q=*&type=dataverse"
+        dataverse_ids = [data['identifier'] for data in self.compile_paginated_data(dataverse_endpoint)]
+        logger.info(f"found {len(dataverse_ids)} dataverse IDs")
+        datasets = []
+        logger.info("extracting dataverse datasets...")
+        for dv_id in dataverse_ids:
+            dv_id_query = f"{self.DATAVERSE_SERVER}/search?q=*&type=dataset&subtree={dv_id}"
+            datasets_and_files = self.compile_paginated_data(dv_id_query)
+            datasets.extend(datasets_and_files)
+        logger.info(f"{len(datasets)} dataverse datasets extracted")
+        return datasets
+
+
+    def scrape_schema_representation(self, url):
+        """
+        when the schema.org export of the dataset fails
+        this will grab it from the url
+        by looking for <script type="application/ld+json">
+        """
+        class SchemaScraper(HTMLParser):
+            def __init__(self):
+                super().__init__()
+                self.readingSchema = False
+                self.schema = None
+
+            def handle_starttag(self, tag, attrs):
+                if tag == 'script' and 'type' in attrs and attrs.get('type') == "application/ld+json":
+                    self.readingSchema = True
+                    print('herexs')
+
+            def handle_data(self, data):
+                if self.readingSchema:
+                    self.schema = data
+                    self.readingSchema = False
+        try:
+            req = requests.get(url)
+        except Exception as requestException:
+            logger.error(f"Failed to get {url} due to {requestException}")
+            return False
+        if not req.ok:
+            logger.error(f"failed to get {url}")
+            return False
+        parser = SchemaScraper()
+        parser.feed(req.text)
+        if parser.schema:
+            return parser.schema
+        return False
+
+
+    def get_schema_document(self, gid, url, verbose=False):
+        if "https://hdl.handle.net/" in url:
+            return False
+        else:
+            schema_export_url = f"{self.EXPORT_URL}&persistentId={gid}"
+            if verbose == True:
+                logger.info(f"exporting schema.org document for {gid}")
+            try:
+                req = requests.get(schema_export_url)
+            except Exception as requestException:
+                logger.error(f"Failed to get {schema_export_url} due to {requestException}")
+                return False
+            try:
+                res = req.json()
+            except json.decoder.JSONDecodeError:
+                return False
+            if res.get('status') and res.get('status') == 'ERROR':
+                logger.warning(f"document export failed, scraping {url} instead")
+                document = self.scrape_schema_representation(url)
+                if document:
+                    logger.info("successfully scrapped document")
+                    return document
+                else:
+                    return False
+            else:
+                # success, response is the document
+                logger.info(f"schema export successful {gid}")
+                return res
+
+
+    def compile_paginated_data(self, query_endpoint, per_page=10, verbose=False):
+        """ Extract Data By Page
+        pages through data, compiling all response['data']['items']
+        and returning them.
+        per_page max is 1000
+        """
+        continue_paging = True
+        start   = 0
+        retries = 0
+        data_pages = []
+        
+        while continue_paging:
+            url = f"{query_endpoint}&per_page={per_page}&start={start}"
+            if verbose == True:
+                logger.info(f"querying endpoint {url}")
+            try:
+                req = requests.get(url)
+            except Exception as requestException:
+                logger.error(f"Failed to get {url} due to {requestException}")
+                if retries > 5:
+                    logger.error(f"Failed too many times on endpoint {url}")
+                retries += 1
+                # after 1 retry, limit per-page to 200, after 2, limit to 50
+                per_page = 200 if retries == 1 else 50
+            try:
+                response = req.json()
+            except ValueError:
+                logger.error(f"Failed to get a JSON response from {url}") 
+            total = response.get('data').get('total_count')
+            page_data = response.get('data').get('items')
+            start += per_page
+            data_pages.extend(page_data)
+            continue_paging = total and start < 50# total
+        return data_pages
+
+
+    def fetch_datasets(self):
+        """Retrives the raw data using a sickle request and formats so dump can store it into the cache table
+        Returns:
+            A tuple (_id, data formatted as a json string)
+        grabs all datasets and files related to QUERIES both by querying
+        and by grabbing everything in related dataverses
+        extracts their global_id, which in this case is a DOI
+        returns a dictionary mapping global_id -> dataset
+        """
+        dataset_ids = set([None])
+        datasets    = []
+
+        logger.info("starting extraction of all search data")
+        new_data = self.compile_paginated_data(self.DATA_URL)
+        unique_data = [dataset for dataset in new_data if dataset.get('global_id') not in dataset_ids]
+        datasets.extend(unique_data)
+        logger.info(f"{len(unique_data)} unique search dataset records extracted")
+
+        dataset_ids |= set([dataset.get('global_id') for dataset in unique_data])
+        data_for_gid = {d.get('global_id'): d for d in datasets}
+
+        logger.info("starting extraction of all dataverse data")
+        dataverse_data = self.get_all_datasets_from_dataverses()
+        dataverse_data_for_gid = {d.get('global_id'): d for d in dataverse_data}
+
+        total_datasets = {
+            **data_for_gid,
+            **dataverse_data_for_gid
+        }
+
+        try:
+            total_datasets.pop('')
+        except KeyError:
+            pass
+        return total_datasets
+
+
+    def load_cache(self):
+        logger.info("Loading cache....")
+        start_time = time.process_time()
+        datasets = self.fetch_datasets()
+        record_ct = 0
+        # get the schemas for the data
+        for gid, data in datasets.items():
+            schema_record = self.get_schema_document(gid, data.get('url'))
+            if schema_record:
+                record_ct += 1
+                yield (schema_record['@id'], json.dumps(schema_record))
+        process_time = time.process_time() - start_time
+        logger.info(f"load cache successful, {record_ct} datasets loaded in {process_time:.2f} seconds")
+
+    def parse(self, records):
+        
+        start_time = time.process_time()
+        logger.info(f"Starting metadata parser on {len(records)} records...")
+        count = 0
+
+        for data_obj in records:
+            dataset=json.loads(data_obj[1])
+
+            dataset['dateModified'] = datetime.datetime.strptime(dataset['dateModified'] , "%Y-%m-%d").date().isoformat()
+            dataset['datePublished'] = datetime.datetime.strptime(dataset['datePublished'] , "%Y-%m-%d").date().isoformat()
+
+            dataset['url'] = dataset.pop('identifier')
+            dataset['doi'] = dataset['url'].replace("/","_").replace('https:__doi.org','Dataverse')
+
+            if dataset['author'] is None:
+                dataset.pop('author')
+                if dataset['creator']:
+                    dataset['author'] = dataset.pop('creator')
+                else:
+                    dataset.pop('creator')
+            else:
+                dataset.pop('creator')
+
+            if dataset['publisher'] is None:
+                dataset.pop('publisher')
+                if dataset['provider']:
+                    dataset['publisher'] = dataset.pop('provider')
+                else:
+                    dataset.pop('provider')
+            else:
+                dataset.pop('provider')
+            
+            if "funder" in dataset.keys():
+                dataset['funding'] = {"funder": dataset.pop('funder')}
+
+            count += 1
+
+            yield dataset
+        process_time = time.process_time() - start_time
+        logger.info(f"completed parsing individual metadata, {count} records parsed in {process_time:.2f} seconds")
+
+# if __name__ == '__main__':
+#     obj = Dataverse()
+#     cache_file_data = []
+#     for rec in obj.load_cache():
+#         cache_file_data.append(rec)
+#         print(rec)
+#     #print(len(cache_file_data))
+#     with open('load_cache_testing.json', 'w') as output:
+#         json.dump([data for data in cache_file_data[:20]], output, indent=4)
+#     parse_file_data = []
+#     for rec in obj.parse():
+#         parse_file_data.append(rec)
+#         print(json.dumps(rec, indent=4))
+#     #print(len(parse_file_data))
+#     with open('parse_output_test.json', 'w') as output:
+#         json.dump([data for data in parse_file_data[:20]], output, indent=4)
---
+++ dataverse/files/sql_database.py
@@ -0,0 +1,200 @@
+import sqlite3
+import datetime
+import logging
+import os
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(name)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger('nde-logger')
+
+class NDEDatabase:
+    # override in subclass
+    # how many days before cache expires
+    EXPIRE = datetime.timedelta(days=30)
+    # name of database
+    SQL_DB = None
+    # Never uses cache, defaults as false
+    NO_CACHE = False
+
+    def __init__(self, sql_db=None):
+        # database name example: zenodo.db
+        self.SQL_DB = self.SQL_DB or sql_db
+        # parses directory name from database name
+        self.DIR_NAME = self.SQL_DB.split('.')[0]
+        # puts directory into cache folder
+        self.path = os.path.join('/cache/', self.DIR_NAME)
+        # make the directory
+        os.makedirs(self.path, exist_ok=True)
+
+    def is_cache_expired(self):
+        """ Uses the SQL_DB to connect to a sqlite db to check if the cache is expired in metadata table
+            Returns:
+                True: if cache does not exist or is expired, False otherwise
+        """
+
+        # connect to database
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+
+        # SQLite table names are case insensitive, but comparison is case sensitive by default.
+        # To make this work properly in all cases you need to add COLLATE NOCASE.
+        c.execute(
+            """SELECT name FROM sqlite_master WHERE type='table' AND name='metadata' COLLATE NOCASE""")
+
+        # if the table exists check date_created if not return True
+        if c.fetchone():
+            c.execute("""SELECT date FROM metadata WHERE name='date_created'""")
+            date_created = c.fetchall()
+            assert len(
+                date_created) <= 1, "There is more than one date_created."
+            date_created = datetime.date.fromisoformat(date_created[0][0])
+            today = datetime.date.today()
+            # compare if date is expired
+            if today - date_created < self.EXPIRE:
+                con.close()
+                return False
+        con.close()
+        return True
+
+    def new_cache(self):
+        """Creates a new tables: metadata and cache. Upserts two entries: date_created, date_updated in metadata table"""
+
+        # Read comments in base class for the first part
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+
+        logger.info("Cache does not exist or is expired. Creating new cache.")
+
+        c.execute("""CREATE TABLE IF NOT EXISTS metadata (
+                name text NOT NULL PRIMARY KEY,
+                date text NOT NULL
+                )""")
+
+        today = datetime.date.today().isoformat()
+
+        # used for testing
+        # today = datetime.date(2022, 6, 7).isoformat()
+
+        # upserting in sqlite https://www.sqlite.org/lang_UPSERT.html
+        # https://stackoverflow.com/questions/62274285/sqlite3-programmingerror-incorrect-number-of-bindings-supplied-the-current-stat
+        c.execute("""INSERT INTO metadata VALUES(?, ?)
+                        ON CONFLICT(name) DO UPDATE SET date=excluded.date
+                  """, ('date_created', today))
+
+        # add date_updated
+        c.execute("""INSERT INTO metadata VALUES(?, ?)
+                        ON CONFLICT(name) DO UPDATE SET date=excluded.date
+                  """, ('date_updated', today))
+        con.commit()
+
+        c.execute("DROP TABLE IF EXISTS cache")
+        c.execute("""CREATE TABLE cache (
+                      _id text NOT NULL PRIMARY KEY,
+                      data text NOT NULL
+                     )""")
+        con.commit()
+
+        con.close()
+
+    def load_cache(self):
+        """Download the request information"""
+        raise NotImplementedError("Define in subclass")
+
+    def update_cache(self):
+        """Update cache with new data"""
+        pass
+
+    def retreive_last_updated(self):
+        """Helper method for update_cache. Gets the date_updated value from metadata table."""
+
+        # connect to database
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+
+        # checks date_updated
+        c.execute("SELECT date from metadata WHERE name='date_updated'")
+        last_updated = c.fetchall()
+        assert len(last_updated) <= 1, "There is more than one last_updated."
+        last_updated = last_updated[0][0]
+
+        con.close()
+
+        return last_updated
+
+    def insert_last_updated(self):
+        """Helper method for update_cache. Changes the date_updated in the metadata table to today"""
+        # connect to database
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+
+        today = datetime.date.today().isoformat()
+        # upsert date_updated to today
+        c.execute("""INSERT INTO metadata VALUES(?, ?)
+                        ON CONFLICT(name) DO UPDATE SET date=excluded.date
+                  """, ('date_updated', today))
+
+        logger.info("Cache last_updated on: %s", today)
+        con.commit()
+
+        con.close()
+
+    def dump(self, records):
+        """Stores raw data from a list or generator into the cache table. Each value of the list contains (_id, data)"""
+
+        # connect to database
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+
+        logger.info("Dumping records...")
+        for record in records:
+            _id = record[0]
+            data = record[1]
+            if not (isinstance(_id, str) and isinstance(data, str)):
+                raise TypeError("_id and data must be a string")
+            # insert doc into cache table _id first column second column data as a json string
+            # we upsert here in case there is repeat ids from existing cache
+            c.execute("""INSERT INTO cache VALUES(?, ?)
+                            ON CONFLICT(_id) DO UPDATE SET data=excluded.data
+                        """, (_id, data))
+            con.commit()
+
+        logger.info("Finished dumping records.")
+
+        con.close()
+
+    def retreive_cache(self):
+        con = sqlite3.connect(self.path + '/' + self.SQL_DB)
+        c = con.cursor()
+        c.execute("SELECT * from cache")
+        records = c.fetchall()
+        con.close()
+        return records
+
+    def parse(self, records):
+        """Parse the request information"""
+        raise NotImplementedError("Define in subclass")
+
+    def upload(self):
+        """Checks if cache is expired or NO_CACHE is True if so make a new cache and dump else update existing cache"""
+        if self.is_cache_expired() or self.NO_CACHE:
+            # make new cache
+            self.new_cache()
+            # api request to get all the data in a generator. Each entry (_id, data) -> (str, str)
+            records = self.load_cache()
+            # dumps all records into the cache table of sql database
+            self.dump(records)
+            # retreives all the records from the cache table
+            records = self.retreive_cache()
+            # pipeline to transform data to put into the ndjson file
+            return self.parse(records)
+        else:
+            # gets new records
+            records = self.update_cache()
+            # dumps all new records into cache table that has existing data
+            self.dump(records)
+            # retreives all the records from the cache table
+            records = self.retreive_cache()
+            # pipeline to transform data to put into the ndjson file
+            return self.parse(records)
