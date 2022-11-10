59fbe5f940b22433e57d6deba5f9e3f126215927
Nichollette <nichollette.aco@gmail.com>
2022-11-09 21:27:21 -0500

    update for full dataverse, build was successful

--- biothings-hub/files/nde-hub/hub/dataload/sources/dataverse/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/dataverse/uploader.py
@@ -1,4 +1,22 @@
 from hub.dataload.nde import NDESourceUploader
+from utils.pmid_helper import load_pmid_ctfd
+from utils.csv_helper import get_source_data
 
 class DataverseUploader(NDESourceUploader):
     name = "dataverse"
+    __metadata__ = {
+        "src_meta": {
+            "sourceInfo": {
+                "description": "The Harvard Dataverse Repository is a free data repository open to all researchers from any discipline, both inside and outside of the Harvard community, where you can share, archive, cite, access, and explore research data. Each individual Dataverse collection is a customizable collection of datasets (or a virtual repository) for organizing, managing, and showcasing datasets.",
+                "identifier": "Dataverse",
+                "name": "Dataverse"",
+                "schema": get_source_data(name),
+                "url": "https://dataverse.harvard.edu/"
+            }
+        }
+    }
+
+    def load_data(self, data_folder):
+        docs = load_pmid_ctfd(data_folder)
+        for doc in docs:
+            yield doc
