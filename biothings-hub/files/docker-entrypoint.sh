c038afe363cbbe359f56205726754cc2c4943d6e
Jason Lin <jalin@su07.scripps.edu>
2023-01-09 14:43:08 -0800

    testing new hub branch

--- biothings-hub/files/docker-entrypoint.sh
+++ biothings-hub/files/docker-entrypoint.sh
@@ -4,3 +4,4 @@ cd /home/biothings/nde-hub
 # replace PID 1 process with Python so it will receive the SIGINT when
 # people run `docker stop nde-hub`
 PYTHONPATH=/home/biothings/nde-hub exec /home/biothings/venv/bin/python /home/biothings/nde-hub/bin/hub.py
+# PYTHONPATH=/home/biothings/nde-hub exec /home/biothings/venv/bin/python -m cProfile -o /home/biothings/hub_profiling.txt /home/biothings/nde-hub/bin/hub.py
