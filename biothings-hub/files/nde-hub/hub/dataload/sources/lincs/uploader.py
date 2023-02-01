fa038303fe2fbcc9f639eee3f725345da2334f3e
Nichollette <nichollette.aco@gmail.com>
2023-02-01 16:42:22 -0500

    csv file added

--- biothings-hub/files/nde-hub/hub/dataload/sources/lincs/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/lincs/uploader.py
@@ -8,8 +8,8 @@ class LINCSUploader(NDESourceUploader):
         "src_meta": {
             "sourceInfo": {
                 "description": "The BD2K-LINCS DCIC is comprised of four major components: Integrated Knowledge Environment (IKE), Data Science Research (DSR), Community Training and Outreach (CTO) and Consortium Coordination and Administration (CCA). The Center is constructing a high-capacity scalable integrated knowledge environment enabling federated access, intuitive querying and integrative analysis and visualization across all LINCS resources and many additional external data types from other relevant resources. The Center’s data science research projects are aimed at addressing various data integration and intracellular molecular regulatory network challenges. The Center aims to develop: 1) methods to connect cellular and organismal phenotypes with molecular cellular signatures, and 2) novel data visualization methods for dynamically interacting with large-genomics and proteomics datasets.",
-                "identifier": "BD2K-LINCS DCIC",
-                "name": "lincs",
+                "identifier": "LINCS",
+                "name": "BD2K-LINCS DCIC",
                 "schema": get_source_data(name),
                 "url": "https://lincsportal.ccs.miami.edu/"
             }
