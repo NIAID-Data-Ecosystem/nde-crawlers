225a28591f42164edbad57cded38ddfdfd52bdcf
Jason Lin <jalin@su07.scripps.edu>
2023-04-05 13:34:35 -0700

    temp fix for zenodo

--- biothings-hub/files/nde-hub/hub/dataload/sources/zenodo/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/zenodo/uploader.py
@@ -1,5 +1,16 @@
+import os
+import orjson
+from utils.date import add_date
 from hub.dataload.nde import NDESourceUploader
 
 
 class ZenodoUploader(NDESourceUploader):
     name = "zenodo"
+
+    def load_data(self, data_folder):
+        with open(os.path.join(data_folder, 'data.ndjson'), 'rb') as f:
+            for line in f:
+                doc = orjson.loads(line)
+                # add date transformation here and have the most recent date of all of the dates
+                doc = add_date(doc)
+                yield doc
