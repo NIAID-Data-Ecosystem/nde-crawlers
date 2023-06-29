38690e8ba5d9027a19c52ef15ee05dd58660be4b
Jason Lin <35415519+jal347@users.noreply.github.com>
2023-06-29 14:25:17 -0700

    Update uploader.py

--- biothings-hub/files/nde-hub/hub/dataload/sources/zenodo/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/zenodo/uploader.py
@@ -1,13 +1,13 @@
 import os
 import orjson
-from utils.utils import check_schema
+from utils.date import add_date
 from hub.dataload.nde import NDESourceUploader
 
 
 class ZenodoUploader(NDESourceUploader):
     name = "zenodo"
 
-    @check_schema
+    @add_date
     def load_data(self, data_folder):
         with open(os.path.join(data_folder, 'data.ndjson'), 'rb') as f:
             for line in f:
