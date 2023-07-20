d9b1329249cae36de230201e16b0e58f47803c07
jal347 <linjason.03@gmail.com>
2023-07-20 15:23:14 -0700

    added comments on how to continue dump process if cut short by a hub restart

--- dockstore/files/run-api-crawler.sh
+++ dockstore/files/run-api-crawler.sh
@@ -6,6 +6,7 @@
     cd /home/biothings || exit
     export PYTHONPATH="."
     # only one instance will be running at a time
+    # If the hub is restarted and we need to connect back to the container, this command will wait until the container is finished running and continue the dump process. 
     flock --verbose --nonblock /crawler.lock \
       su -c '/home/biothings/venv/bin/python3.10 ndjson.py' biothings ||
       flock --verbose /crawler.lock -c 'echo "failed to acquire lock, skipping crawling process"'
