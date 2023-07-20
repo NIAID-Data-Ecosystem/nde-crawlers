90833d428d9497482fc2e0e3fdd15a5e7f8042b0
jal347 <linjason.03@gmail.com>
2023-07-20 15:06:51 -0700

    added comments for more context on why we need keep_container=true in sql cached plugin cases and extra information on how to use the docker-compose-crawlers.yml

--- biothings-hub/files/nde-hub/hub/dataload/sources/figshare/dumper.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/figshare/dumper.py
@@ -18,6 +18,7 @@ class FigshareDumper(DockerContainerDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = None
     UNCOMPRESS = True
+    # We need to set keep_container = true in order to retrieve cached data used in dump_command
     SRC_URLS = [
         f'docker://su07?image=nde-crawlers_{SRC_NAME}-crawler&tag=latest&path=/data/{SRC_NAME}_crawled/data.ndjson&dump_command="/home/biothings/run-api-crawler.sh"&container_name={SRC_NAME}_dumper&keep_container=true'
         # &custom_cmd="/usr/bin/wget https://s3.pgkb.org/data/annotations.zip -O /tmp/annotations.zip"'
