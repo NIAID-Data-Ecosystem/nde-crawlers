aebeaf085ea305519beb0bc764fc7fbf98847deb
jal347 <linjason.03@gmail.com>
2023-08-15 10:06:04 -0700

    added NDEDataBuilder

---
+++ biothings-hub/files/nde-hub/hub/databuild/builder.py
@@ -0,0 +1,12 @@
+import biothings.hub.databuild.builder as builder
+
+
+class NDEDataBuilder(builder.DataBuilder):
+    def merge_order(self, other_sources):
+        self.logger.info("Other sources: %s", other_sources)
+        """Merge order for NDE data sources. Highest priority is merged last"""
+        priority = ["zenodo"]
+        for source in reversed(priority):
+            other_sources.append(other_sources.pop(other_sources.index(source)))
+        self.logger.info("This is the merge order: %s", other_sources)
+        return other_sources
