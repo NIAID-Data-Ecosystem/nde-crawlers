fe74a106c02af1c75a3bf15acccf77ecc34bfa9e
Dylan Welzel <dylanwelzel@gmail.com>
2023-11-28 09:24:03 -0700

    Update uploader.py
    omicsdi species and disease augmentation

--- biothings-hub/files/nde-hub/hub/dataload/sources/omicsdi/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/omicsdi/uploader.py
@@ -1,6 +1,7 @@
 from hub.dataload.nde import NDESourceUploader
 from utils.funding_helper import standardize_funding
 from utils.pmid_helper import load_pmid_ctfd
+from utils.pubtator import standardize_data
 from utils.utils import nde_upload_wrapper
 
 
@@ -16,5 +17,6 @@ class OmicsDIUploader(NDESourceUploader):
     def load_data(self, data_folder):
         docs = load_pmid_ctfd(data_folder)
         docs = standardize_funding(docs)
+        docs = standardize_data(docs)
         for doc in docs:
             yield doc
