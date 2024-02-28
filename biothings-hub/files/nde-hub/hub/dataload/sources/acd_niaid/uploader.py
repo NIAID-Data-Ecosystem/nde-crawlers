0fd76e543b250d51aa3b5f2ce4d82f782331708f
Jason Lin <jalin@su07.scripps.edu>
2024-02-28 10:49:59 -0800

    new branch to test volume flags

--- biothings-hub/files/nde-hub/hub/dataload/sources/acd_niaid/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/acd_niaid/uploader.py
@@ -13,10 +13,10 @@ class AccessClinicalDataUploader(NDESourceUploader):
         "merger_kwargs": {"aslistofdict": "includedInDataCatalog", "include": ["includedInDataCatalog"]},
     }
 
-    @nde_upload_wrapper
-    def load_data(self, data_folder):
-        docs = load_pmid_ctfd(data_folder)
-        docs = standardize_funding(docs)
-        docs = standardize_data(docs)
-        for doc in docs:
-            yield doc
+    # @nde_upload_wrapper
+    # def load_data(self, data_folder):
+    #     docs = load_pmid_ctfd(data_folder)
+    #     docs = standardize_funding(docs)
+    #     docs = standardize_data(docs)
+    #     for doc in docs:
+    #         yield doc
