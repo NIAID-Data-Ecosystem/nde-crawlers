20220da6402218bb76e0d84851df83fac04d2d4d
Jason Lin <jalin@su07.scripps.edu>
2024-02-28 10:49:59 -0800

    new branch to test volume flags

--- biothings-hub/files/nde-hub/hub/dataload/sources/acd_niaid/dumper.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/acd_niaid/dumper.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 
 import biothings
@@ -18,7 +19,11 @@ class AccessClinicalDataDumper(DockerContainerDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = None
     UNCOMPRESS = True
+    NAMED_VOLUMES = {"name": "myvolume", "driver":"local", "driver_opts": {"type": "none", "o": "bind", "device": "/opt/nde/data_test_1"}}
+    NAMED_VOLUMES = json.dumps(NAMED_VOLUMES)
+    VOLUMES = {"myvolume":{"bind": "/data"}}
+    VOLUMES = json.dumps(VOLUMES)
     SRC_URLS = [
-        f'docker://su07?image=nde-crawlers_{SRC_NAME}-crawler&tag=latest&path=/data/{SRC_NAME}_crawled/data.ndjson&dump_command="/home/biothings/run-api-crawler.sh"&container_name={SRC_NAME}_dumper&keep_container=false'
+        f'docker://su07?image=nde-crawlers_{SRC_NAME}-crawler&tag=latest&path=/data/{SRC_NAME}_crawled/data.ndjson&dump_command="/home/biothings/run-api-crawler.sh"&container_name={SRC_NAME}_dumper&keep_container=false&volumes={VOLUMES}&named_volumes={NAMED_VOLUMES}'
         # &custom_cmd="/usr/bin/wget https://s3.pgkb.org/data/annotations.zip -O /tmp/annotations.zip"'
     ]
--- biothings-hub/files/nde-hub/hub/dataload/sources/dataverse/dumper.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/dataverse/dumper.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 
 import biothings
@@ -18,8 +19,15 @@ class DataverseDumper(DockerContainerDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = None
     UNCOMPRESS = True
+    NAMED_VOLUMES = [{"name": "dataverse_data", "driver":"local", "driver_opts": {"type": "none", "o": "bind", "device": "/opt/nde/data_test_1"}},
+                     {"name": "dataverse_cache", "driver":"local", "driver_opts": {"type": "none", "o": "bind", "device": "/opt/nde/cache_test"}}]
+    NAMED_VOLUMES = json.dumps(NAMED_VOLUMES)
+    # VOLUMES = {"/opt/nde/data_test_1": {"bind": "/data", "mode": "rw"}, "/opt/nde/cache_test": {"bind": "/cache", "mode": "rw"}}
+    VOLUMES = {"dataverse_data":{"bind": "/data"}, "dataverse_cache": {"bind": "/cache"}}
+    VOLUMES = json.dumps(VOLUMES)
+
     # We need to set keep_container = true in order to retrieve cached data used in dump_command
     SRC_URLS = [
-        f'docker://su07?image=nde-crawlers_{SRC_NAME}-crawler&tag=latest&path=/data/{SRC_NAME}_crawled/data.ndjson&dump_command="/home/biothings/run-api-crawler.sh"&container_name={SRC_NAME}_dumper&keep_container=true'
+        f'docker://su07?image=nde-crawlers_{SRC_NAME}-crawler&tag=latest&path=/data/{SRC_NAME}_crawled/data.ndjson&dump_command="/home/biothings/run-api-crawler.sh"&container_name={SRC_NAME}_dumper&volumes={VOLUMES}&named_volumes={NAMED_VOLUMES}'
         # &custom_cmd="/usr/bin/wget https://s3.pgkb.org/data/annotations.zip -O /tmp/annotations.zip"'
     ]
