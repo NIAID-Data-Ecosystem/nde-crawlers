f5b8abac46769c8963ba1206e046dc72f313486b
Dylan Welzel <dylanwelzel@gmail.com>
2024-06-05 10:55:15 -0700

    add helper to ncbi_sra uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_sra/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_sra/uploader.py
@@ -1,17 +1,13 @@
 from hub.dataload.nde import NDESourceUploader
-from utils.csv_helper import get_source_data
+from utils.pubtator import standardize_data
+from utils.utils import nde_upload_wrapper
 
 
 class NCBI_SRA_Uploader(NDESourceUploader):
     name = "ncbi_sra"
-    __metadata__ = {
-        "src_meta": {
-            "sourceInfo": {
-                "description": "Sequence Read Archive(SRA) data, available through multiple cloud providers and NCBI servers, is the largest publicly available repository of high throughput sequencing data. The archive accepts data from all branches of life as well as metagenomic and environmental surveys. SRA stores raw sequencing data and alignment information to enhance reproducibility and facilitate new discoveries through data analysis.",
-                "identifier": "NCBI SRA",
-                "name": "NCBI SRA",
-                "schema": get_source_data(name),
-                "url": "https://www.ncbi.nlm.nih.gov/sra",
-            }
-        }
-    }
+
+    @nde_upload_wrapper
+    def load_data(self, data_folder):
+        docs = standardize_data(data_folder)
+        for doc in docs:
+            yield doc
