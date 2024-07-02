67ca5cfa2a9c027fbe2baf587e7d7d31a21fe26b
Dylan Welzel <dylanwelzel@gmail.com>
2024-07-02 10:49:21 -0700

    fix hubmap variable

--- biothings-hub/files/nde-hub/hub/dataload/sources/hubmap/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/hubmap/uploader.py
@@ -33,7 +33,7 @@ class Hubmap_Uploader(NDESourceUploader):
 
     @nde_upload_wrapper
     def load_data(self, data_folder):
-        docs = process_descriptions(docs)
-        docs = add_topic_category(data_folder, self.name)
+        docs = process_descriptions(data_folder)
+        docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
