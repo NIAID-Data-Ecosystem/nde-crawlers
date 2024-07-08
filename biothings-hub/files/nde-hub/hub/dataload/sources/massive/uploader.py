2c81238d5b544b96f42c8e52455ad97ac9c0b671
Dylan Welzel <dylanwelzel@gmail.com>
2024-07-08 13:22:35 -0700

    add extract to massive

--- biothings-hub/files/nde-hub/hub/dataload/sources/massive/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/massive/uploader.py
@@ -1,4 +1,5 @@
 from hub.dataload.nde import NDESourceUploader
+from utils.extract import process_descriptions
 from utils.pubtator import standardize_data
 from utils.utils import nde_upload_wrapper
 
@@ -9,5 +10,6 @@ class MassiveUploader(NDESourceUploader):
     @nde_upload_wrapper
     def load_data(self, data_folder):
         docs = standardize_data(data_folder)
+        docs = process_descriptions(docs)
         for doc in docs:
             yield doc
