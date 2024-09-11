e98112106a4d59ac48d41e44d053164f8ffa3faf
Dylan Welzel <dylanwelzel@gmail.com>
2024-09-11 09:51:06 -0700

    fix utils in mendeley uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/mendeley/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/mendeley/uploader.py
@@ -12,6 +12,6 @@ class MendeleyUploader(NDESourceUploader):
     def load_data(self, data_folder):
         docs = process_descriptions(data_folder)
         docs = standardize_funding(docs)
-        docs = add_topic_category(data_folder, self.name)
+        docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
