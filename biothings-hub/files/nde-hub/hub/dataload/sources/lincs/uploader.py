3d37b1edc101520fb0d7e34a5f2e0aede7ce4ff7
Dylan Welzel <dylanwelzel@gmail.com>
2024-10-28 12:03:58 -0700

    add measurement technique util to ncbi geo

--- biothings-hub/files/nde-hub/hub/dataload/sources/lincs/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/lincs/uploader.py
@@ -27,7 +27,7 @@ class LINCSUploader(NDESourceUploader):
     def load_data(self, data_folder):
         docs = standardize_funding(data_folder)
         docs = process_descriptions(docs)
-        docs = process_measurement_technique(data_folder)
+        docs = process_measurement_technique(docs)
         docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/uploader.py
@@ -1,6 +1,7 @@
 from hub.dataload.nde import NDESourceUploader
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
+from utils.measurement_technique_helper import process_measurement_technique
 from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
@@ -25,6 +26,7 @@ class NCBI_Geo_Uploader(NDESourceUploader):
         docs = standardize_funding(docs)
         docs = standardize_data(docs)
         docs = process_descriptions(docs)
+        docs = process_measurement_technique(docs)
         docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
