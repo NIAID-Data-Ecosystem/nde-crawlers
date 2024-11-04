743f6b3a7099af0955a8e6ad00dc3cc4a5adc5c6
Dylan Welzel <dylanwelzel@gmail.com>
2024-11-04 12:49:13 -0800

    add lineage info to dde

--- biothings-hub/files/nde-hub/hub/dataload/sources/dde/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/dde/uploader.py
@@ -2,6 +2,7 @@ from hub.dataload.nde import NDESourceUploader
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
 from utils.in_defined_term_set import handle_dde_docs
+from utils.lineage import process_lineage
 from utils.pmid_helper import load_pmid_ctfd, standardize_fields
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
@@ -26,6 +27,7 @@ class DDEUploader(NDESourceUploader):
         docs = standardize_funding(docs)
         docs = standardize_data(docs)
         docs = process_descriptions(docs)
+        docs = process_lineage(docs)
         docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
