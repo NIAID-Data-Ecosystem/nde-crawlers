cfe43f6f005cb29e4ec26225d8d5f05c7413f301
Dylan Welzel <dylanwelzel@gmail.com>
2025-01-13 13:23:00 -0800

    add extract pipeline to biotools

--- biothings-hub/files/nde-hub/hub/dataload/sources/biotools/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/biotools/uploader.py
@@ -1,7 +1,8 @@
 from hub.dataload.nde import NDESourceUploader
-from utils.csv_helper import get_source_data
+from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
 from utils.utils import nde_upload_wrapper
+
 
 class Biotools_Uploader(NDESourceUploader):
     name = "biotools"
@@ -9,5 +10,6 @@ class Biotools_Uploader(NDESourceUploader):
     @nde_upload_wrapper
     def load_data(self, data_folder):
         docs = standardize_funding(data_folder)
+        docs = process_descriptions(docs)
         for doc in docs:
             yield doc
