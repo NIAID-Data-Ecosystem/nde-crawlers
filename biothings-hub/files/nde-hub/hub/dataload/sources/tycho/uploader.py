cc646a46e8539b12fcb9b699fefcd8b7cd0db614
Jason Lin <jalin@su07.scripps.edu>
2025-02-28 09:59:11 -0800

    added helpers to tycho

--- biothings-hub/files/nde-hub/hub/dataload/sources/tycho/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/tycho/uploader.py
@@ -1,5 +1,18 @@
 from hub.dataload.nde import NDESourceUploader
+from utils.corrections import corrections
+from utils.funding_helper import standardize_funding
+from utils.pubtator import standardize_data
+from utils.utils import nde_upload_wrapper
 
 
 class TychoUploader(NDESourceUploader):
     name = "tycho"
+
+
+    @nde_upload_wrapper
+    def load_data(self, data_folder):
+        docs = standardize_funding(docs)
+        docs = standardize_data(docs)
+        docs = corrections(docs)
+        for doc in docs:
+            yield doc
