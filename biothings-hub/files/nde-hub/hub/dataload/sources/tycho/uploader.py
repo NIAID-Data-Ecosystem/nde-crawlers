30feb0ad31c8882ca54feeae98d8ea9afe7facb5
Dylan Welzel <dylanwelzel@gmail.com>
2025-02-28 10:06:13 -0800

    add corrections to util decorator

--- biothings-hub/files/nde-hub/hub/dataload/sources/tycho/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/tycho/uploader.py
@@ -1,5 +1,4 @@
 from hub.dataload.nde import NDESourceUploader
-from utils.corrections import corrections
 from utils.funding_helper import standardize_funding
 from utils.pubtator import standardize_data
 from utils.utils import nde_upload_wrapper
@@ -8,11 +7,9 @@ from utils.utils import nde_upload_wrapper
 class TychoUploader(NDESourceUploader):
     name = "tycho"
 
-
     @nde_upload_wrapper
     def load_data(self, data_folder):
-        docs = standardize_funding(docs)
+        docs = standardize_funding(data_folder)
         docs = standardize_data(docs)
-        docs = corrections(docs)
         for doc in docs:
             yield doc
