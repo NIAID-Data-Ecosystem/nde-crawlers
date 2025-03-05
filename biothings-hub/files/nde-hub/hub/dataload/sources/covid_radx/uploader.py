55e71c0b1bef0cd6fb6a5cb9219c5e26f173f30d
Dylan Welzel <dylanwelzel@gmail.com>
2025-03-05 12:36:51 -0700

    add mt util to radx and dbgap

--- biothings-hub/files/nde-hub/hub/dataload/sources/covid_radx/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/covid_radx/uploader.py
@@ -2,6 +2,7 @@ from hub.dataload.nde import NDESourceUploader
 from utils.corrections import corrections
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
+from utils.measurement_technique_helper import process_measurement_technique
 from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
@@ -18,6 +19,7 @@ class Covid_Radx_Uploader(NDESourceUploader):
         docs = standardize_data(docs)
         docs = process_descriptions(docs)
         docs = corrections(docs)
+        docs = process_measurement_technique(docs, self.name)
         docs = add_topic_category(docs, self.name)
         for doc in docs:
             yield doc
--- biothings-hub/files/nde-hub/hub/dataload/sources/dbgap/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/dbgap/uploader.py
@@ -1,6 +1,7 @@
 from hub.dataload.nde import NDESourceUploader
 from utils.corrections import corrections
 from utils.funding_helper import standardize_funding
+from utils.measurement_technique_helper import process_measurement_technique
 from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
@@ -16,6 +17,7 @@ class dbGaP_Uploader(NDESourceUploader):
         docs = standardize_funding(docs)
         docs = standardize_data(docs)
         docs = add_topic_category(docs, self.name)
+        docs = process_measurement_technique(docs, self.name)
         docs = corrections(docs)
         for doc in docs:
             yield doc
