dddfd5ad3c0b265d1c91f74eaed87b1dcd9f3149
Dylan Welzel <dylanwelzel@gmail.com>
2025-03-19 10:48:22 -0700

    add measurementTechnique util to sra

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_sra/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_sra/uploader.py
@@ -2,6 +2,7 @@ from hub.dataload.nde import NDESourceUploader
 from utils.corrections import corrections
 from utils.extract import process_descriptions
 from utils.lineage import process_lineage
+from utils.measurement_technique_helper import process_measurement_technique
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
 from utils.utils import nde_upload_wrapper
@@ -14,6 +15,7 @@ class NCBI_SRA_Uploader(NDESourceUploader):
     def load_data(self, data_folder):
         docs = standardize_data(data_folder)
         docs = process_descriptions(docs)
+        docs = process_measurement_technique(docs, self.name)
         docs = process_lineage(docs)
         docs = corrections(docs)
         docs = add_topic_category(docs, self.name)
