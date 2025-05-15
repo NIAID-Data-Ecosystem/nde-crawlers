3d418f50e60c7e5d77c49377a3a2130c08a36efc
Jason Lin <jalin@su07.scripps.edu>
2025-05-15 16:12:06 -0700

    process species for immunespace

--- biothings-hub/files/nde-hub/hub/dataload/sources/immunespace/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/immunespace/uploader.py
@@ -1,5 +1,6 @@
 from hub.dataload.nde import NDESourceUploader
 from utils.corrections import corrections
+from utils.extract import process_descriptions
 from utils.pubtator import standardize_data
 from utils.utils import nde_upload_wrapper
 
@@ -17,6 +18,7 @@ class ImmunespaceUploader(NDESourceUploader):
     def load_data(self, data_folder):
         docs = standardize_data(data_folder)
         docs = corrections(data_folder)
+        docs = process_descriptions(docs)
 
         for doc in docs:
             yield doc
