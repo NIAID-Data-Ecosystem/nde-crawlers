bfc0a113c68a7fbbf56fcc1970c661d59bc37fb2
jal347 <linjason.03@gmail.com>
2025-06-06 14:57:23 -0700

    fixed pdb uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/pdb/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/pdb/uploader.py
@@ -21,7 +21,6 @@ class PDB_Uploader(NDESourceUploader):
 
     @nde_upload_wrapper
     def load_data(self, data_folder):
-        docs = standardize_data(data_folder)
-        docs = corrections(docs)
+        docs = corrections(data_folder)
         for doc in docs:
             yield doc
