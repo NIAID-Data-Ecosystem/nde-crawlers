006533d77dccff21f097d9387ad11c2a34649340
jal347 <linjason.03@gmail.com>
2025-08-08 16:11:38 -0700

    changed mendeley uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/mendeley/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/mendeley/uploader.py
@@ -2,6 +2,8 @@ from hub.dataload.nde import NDESourceUploader
 from utils.corrections import corrections
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
+from utils.pmid_helper import load_pmid_ctfd
+from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
 from utils.utils import nde_upload_wrapper
 
@@ -11,8 +13,10 @@ class MendeleyUploader(NDESourceUploader):
 
     @nde_upload_wrapper
     def load_data(self, data_folder):
-        docs = process_descriptions(data_folder)
+        docs = load_pmid_ctfd(data_folder)
+        docs = standardize_data(docs)
         docs = standardize_funding(docs)
+        docs = process_descriptions(docs)
         docs = corrections(docs)
         docs = add_topic_category(docs, self.name)
         for doc in docs:
