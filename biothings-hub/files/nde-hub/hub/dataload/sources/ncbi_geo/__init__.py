50701f3db57859657aba6ef0f00418e9439b99e1
Jason Lin <linjason.03@gmail.com>
2025-10-06 12:56:31 -0700

    fixed ncbi-geo init

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/__init__.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/__init__.py
@@ -1,3 +1,3 @@
-from .dumper import New_NCBI_Geo_Dumper  # noqa
+from .dumper import NCBI_Geo_Dumper  # noqa
 from .gse_uploader import GSE_Uploader  # noqa
 from .gsm_uploader import GSM_Uploader  # noqa
