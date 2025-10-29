e966ff68e2a9aa311e78b56da7796d14beabd5c1
jal347 <linjason.03@gmail.com>
2025-10-29 15:26:52 -0700

    removed unnecessary helpers for gsm_uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/gsm_uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/gsm_uploader.py
@@ -1,8 +1,6 @@
 from hub.dataload.nde import NDESourceSampleUploader
 from utils.corrections import corrections
 from utils.extract import process_descriptions
-from utils.funding_helper import standardize_funding
-from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
 from utils.utils import nde_upload_wrapper
@@ -17,8 +15,6 @@ class GSM_Uploader(NDESourceSampleUploader):
     @nde_upload_wrapper
     def load_data(self, data_folder):
         docs = parse_gsm(data_folder)
-        docs = load_pmid_ctfd(docs)
-        docs = standardize_funding(docs)
         docs = standardize_data(docs)
         docs = process_descriptions(docs)
         docs = corrections(docs)
