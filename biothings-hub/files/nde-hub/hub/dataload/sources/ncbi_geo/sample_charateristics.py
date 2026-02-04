e1b807e00e31bab0a81848f87c3410a3d0ac8c01
jal347 <linjason.03@gmail.com>
2026-02-04 13:40:05 -0600

    fixed description issue for sample_characteristics

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/sample_charateristics.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/sample_charateristics.py
@@ -311,3 +311,6 @@ def parse_sample_characteristics(output, value, sample_mapping, nde_mapping, sex
 
         else:
             logger.warning(f"Unmapped sample characteristic subproperty: {subproperty}")
+
+        if description := output.get("description") and isinstance(output["description"], list):
+            output["description"] = " ".join(description)
