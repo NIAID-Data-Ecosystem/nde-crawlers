71c2b219883839a758cd2d6a2643703cea562820
jal347 <linjason.03@gmail.com>
2026-04-10 09:31:53 -0700

    added sex util filter to biosample

--- biothings-hub/files/nde-hub/hub/dataload/sources/biosample/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/biosample/uploader.py
@@ -1,5 +1,12 @@
 from hub.dataload.nde import NDESourceSampleUploader
 
+from .parser import parse_sex
+
 
 class BiosampleUploader(NDESourceSampleUploader):
     name = "biosample"
+
+    def load_data(self, data_folder):
+        docs = parse_sex(data_folder)
+        for doc in docs:
+            yield doc
