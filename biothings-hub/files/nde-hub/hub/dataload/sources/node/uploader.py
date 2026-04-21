c4ae98fc5201d3c3e00596ed4764b7e979d63810
Jason Lin <linjason.03@gmail.com>
2026-04-21 12:22:08 -0700

    test new combined uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/node/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/node/uploader.py
@@ -1,4 +1,4 @@
-from hub.dataload.nde import NDESourceUploader
+from hub.dataload.nde import NDECombinedUploader
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
 from utils.measurement_technique_helper import process_measurement_technique
@@ -8,7 +8,7 @@ from utils.topic_category_helper import add_topic_category
 from utils.utils import nde_upload_wrapper
 
 
-class NODE_Uploader(NDESourceUploader):
+class NODE_Uploader(NDECombinedUploader):
     name = "node"
 
     @nde_upload_wrapper
