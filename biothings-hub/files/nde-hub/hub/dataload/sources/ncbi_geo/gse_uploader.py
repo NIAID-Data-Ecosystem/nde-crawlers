0daffe2880925e32d8963790218a51140cadefe6
Dylan Welzel <dylanwelzel@gmail.com>
2026-05-15 15:25:01 -0700

    Add measurement technique helper to gse upload

--- biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/gse_uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/ncbi_geo/gse_uploader.py
@@ -1,10 +1,12 @@
-from hub.dataload.nde import NDESourceUploader
 from utils.extract import process_descriptions
 from utils.funding_helper import standardize_funding
+from utils.measurement_technique_helper import process_measurement_technique
 from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.topic_category_helper import add_topic_category
 from utils.utils import nde_upload_wrapper
+
+from hub.dataload.nde import NDESourceUploader
 
 from .parser import parse_gse
 
@@ -20,6 +22,7 @@ class GSE_Uploader(NDESourceUploader):
         docs = standardize_funding(docs)
         docs = standardize_data(docs)
         docs = process_descriptions(docs)
+        docs = process_measurement_technique(docs, self.main_source)
         docs = add_topic_category(docs, self.main_source)
         for doc in docs:
             yield doc
