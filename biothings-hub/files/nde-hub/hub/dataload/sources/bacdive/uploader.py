2ab8fc4323a680296d34bc7a9e4c630e6866e2de
jal347 <linjason.03@gmail.com>
2026-06-03 12:03:28 -0500

    fixed bacdive uploader

--- biothings-hub/files/nde-hub/hub/dataload/sources/bacdive/uploader.py
+++ biothings-hub/files/nde-hub/hub/dataload/sources/bacdive/uploader.py
@@ -1,9 +1,10 @@
 from hub.dataload.nde import NDESourceSampleUploader
 from utils.extract import process_descriptions
+from utils.funding_helper import standardize_funding
+from utils.pmid_helper import load_pmid_ctfd
 from utils.pubtator import standardize_data
 from utils.utils import nde_upload_wrapper
-from utils.pmid_helper import load_pmid_ctfd
-from utils.funding_helper import standardize_funding
+
 
 class BacdiveUploader(NDESourceSampleUploader):
     name = "bacdive"
@@ -12,7 +13,7 @@ class BacdiveUploader(NDESourceSampleUploader):
     def load_data(self, data_folder):
         docs = load_pmid_ctfd(data_folder)
         docs = standardize_funding(docs)
-        docs = standardize_data(data_folder)
+        docs = standardize_data(docs)
         docs = process_descriptions(docs)
         for doc in docs:
             yield doc
