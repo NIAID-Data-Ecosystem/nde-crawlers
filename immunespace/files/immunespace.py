422cefc0fcf25a9a97da30bc1e3ef0d323c768b2
Dylan Welzel <dylanwelzel@gmail.com>
2026-08-04 11:56:48 -0700

    add sameAs property structure to NDESourceUploader and update its usage in immunespace.py

--- immunespace/files/immunespace.py
+++ immunespace/files/immunespace.py
@@ -241,7 +241,7 @@ def _subject_dataset(group, crawl_date):
         "identifier": _single_or_list(identifiers),
         "includedInDataCatalog": _catalog(crawl_date),
         "name": primary_study,
-        "sameAs": primary_study.casefold(),
+        "sameAs": {"identifier": primary_study.casefold()},
         "url": STUDY_URL_TEMPLATE.format(study_id=primary_study),
     }
 
